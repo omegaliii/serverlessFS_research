1f67a678c25a3f57b6aab65c650b9256845855bd
JIANHUI LI <jianhuili@JIANHUIs-MacBook-Pro.local>
2018-10-08 11:12:45 -0700

    first version of python in menmory file system

---
+++ inMemoryFileSystem_python/sample.py
@@ -0,0 +1,30 @@
+from FileSystem import *
+
+print("Making two directory under the root.")
+mkdir("/dir1")
+mkdir("/dir2")
+print("list the root:")
+ls("/")
+print("======================================")
+
+print("Creating a test.txt file under dir1")
+create("/dir1/test.txt")
+print("list dir1:")
+ls("/dir1")
+print("======================================")
+
+print("Read test.txt file")
+read("/dir1/test.txt")
+print("Writing dir1")
+write("/dir1/test.txt", "hello \n world")
+print("Read test.txt file again:")
+read("/dir1/test.txt")
+print("Readline test.txt file:")
+readline("/dir1/test.txt")
+print("======================================")
+
+print("Remove dir1.")
+rmdir("/dir1")
+print("list root:")
+ls("/")
+print("======================================")
